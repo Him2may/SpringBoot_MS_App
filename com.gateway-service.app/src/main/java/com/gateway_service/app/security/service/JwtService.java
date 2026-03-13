7fb9479c4cd3afe77f8d7e171e6d05bce554204d
Him <him2may@gmail.com>
2026-03-14 00:51:59 +0530

    added milsec to tokenExpiry check

--- com.gateway-service.app/src/main/java/com/gateway_service/app/security/service/JwtService.java
+++ com.gateway-service.app/src/main/java/com/gateway_service/app/security/service/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
 
     // Check if token is expired
     private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
+        return extractExpiration(token).before(new Date(System.currentTimeMillis()));
     }
 
     // Validate token with userdetails
